59b8277186c8ab89d95e29c442445b3f62e35033
corporateyogi <water.marco@gmx.de>
2017-05-16 17:10:34 +0200

    Add files via upload

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,17 +1,24 @@
-
 //ausfahrbares Menü
 
 var nav = document.querySelector("nav");
+var navigation = nav.querySelectorAll("ul a");
 var menu = false;  //ist das Menü offen?
 var hamburger = document.querySelector("#hamburger");
 
 hamburger.addEventListener("click", function() {
-  if (menu) nav.style.transform = "translateX(0)";
-  else nav.style.transform = "translateX(200px)";
+  (menu) ? nav.style.transform = "translateX(0)" :
+    nav.style.transform = "translateX(200px)";
   menu = !menu;
-})
+});
 
-
+for (var i = 0; i < navigation.length; i++) {
+  navigation[i].addEventListener("click", function() {
+    console.log("navigation clicked!")
+    if (menu) {
+      nav.style.transform = "translateX(0)";
+    menu = false;
+    }
+  });
+}
 
 //Bildergallerie
-
